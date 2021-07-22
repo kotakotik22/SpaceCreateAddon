00f0a2526b6ca4d1d8cafa1e5faa8ea331d690b1
Katte <herokadendoesminecraft@gmail.com>
2021-07-21 19:14:50 -0700

    [COMMIT 0.1] ADDED: Some items and stuff Thats it so far REMOVED: No OTHER: What killed by build.config and bonked my imports? :unprocessormoment:

--- src/main/java/com/kotakotik/coolcreateaddon/register/ModItems.java
+++ src/main/java/com/kotakotik/coolcreateaddon/register/ModItems.java
@@ -2,7 +2,9 @@ package com.kotakotik.coolcreateaddon.register;
 
 import com.simibubi.create.AllItems;
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.repack.registrate.util.entry.ItemEntry;
 import mod.kotakotik.coolcreateaddon.BuildConfig;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 
@@ -13,6 +15,27 @@ public class ModItems {
             return new ItemStack(AllItems.WRENCH.get());
         }
     };
+
+    public static ItemEntry<Item> COAL_POWDER;
+    public static ItemEntry<Item> STEEL_POWDER;
+    public static ItemEntry<Item> STEEL_INGOT;
+    public static ItemEntry<Item> STEEL_NUGGET;
+    public static ItemEntry<Item> STEEL_PLATE;
+
+    public static ItemEntry<Item> ANORTHITE_CRYSTAL;
+    public static ItemEntry<Item> ANORTHITE_POWDER;
+
+    public static ItemEntry<Item> ALLUMINUM_INGOT;
+    public static ItemEntry<Item> ALLUMINUM_NUGGET;
+    public static ItemEntry<Item> ALLUMINUM_PLATE;
+
+    public static ItemEntry<Item> PURPLE_QUARTZ;
+    public static ItemEntry<Item> POLISHED_PURPLE_QUARTZ;
+    public static ItemEntry<Item> INTEGRATED_GUIDANCE_CIRCUIT;
+
+    public static ItemEntry<Item> BLUE_QUARTZ;
+    public static ItemEntry<Item> POLISHED_BLUE_QUARTZ;
+    public static ItemEntry<Item> INTEGRATED_LSS_CIRCUIT;
 
     public static void register(CreateRegistrate registrate) {
         registrate.itemGroup(()->itemGroup, BuildConfig.DISPLAY_NAME);
